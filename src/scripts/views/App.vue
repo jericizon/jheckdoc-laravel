73229b9c1b49d621df5defbeeca8af6d1a0a9d1f
Jeric <im.jericizon@gmail.com>
2020-10-22 15:02:21 +0800

    new build

--- src/scripts/views/App.vue
+++ src/scripts/views/App.vue
@@ -243,7 +243,7 @@
         </ul>
       </div>
     </header>
-    <main class="h-full overflow-y-auto">
+    <main class="h-full overflow-y-scroll">
       <div class="container px-6 mx-auto grid">
 
         <template v-if="showInfo">
