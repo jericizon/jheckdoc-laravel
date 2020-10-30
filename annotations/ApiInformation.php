3c27c0e9d20251ef8591c8662feccb94ec67eb4c
Jeric <im.jericizon@gmail.com>
2020-10-30 20:22:30 +0800

    group annotation

---
+++ annotations/ApiInformation.php
@@ -0,0 +1,19 @@
+<?php
+/*@jheckdocInfo
+{
+    "version": "1.0.0",
+    "title":"Jheckdoc API",
+    "description" : "Sample description of api",
+    "contact" : "hello@jheckdoc.com",
+    "servers": [
+        {
+            "url" : "https://jheckdoc.com",
+            "description": "Production api server"
+        },
+        {
+            "url" : "https://dev.jheckdoc.com",
+            "description": "Development api server"
+        }
+    ]
+}
+*/
